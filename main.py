66f2f2c5c73bcac9ff41777d2f334cbeed911d0a
Pr0xy08 <drew.wandless1@gmail.com>
2025-09-02 20:22:11 +0100

    added additional features including length, lowercase_count, uppercase_count, digit_count, special_count.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import string
 
 # Read the CSV safely
 df = pd.read_csv(
@@ -6,23 +7,28 @@ df = pd.read_csv(
     on_bad_lines="skip",  # skip any malformed lines
     encoding="utf-8",  # ensure proper character handling
     quotechar='"',  # handle quotes inside passwords
-    dtype={'password': str, 'strength': int}  # force types
+    dtype={"password": str, "strength": int}  # force types
 )
 
 # Remove rows with missing passwords, strength or formatting
-df = df.dropna(subset=['password', 'strength'])
-
+df = df.dropna(subset=["password", "strength"])
 # Remove rows with empty passwords
-df = df[df['password'].str.strip() != ""]
+df = df[df["password"].str.strip() != ""]
 # Removes any duplicates
-df = df.drop_duplicates(subset=['password'])
+df = df.drop_duplicates(subset=["password"])
 # reset index
 df = df.reset_index(drop=True)
 
 # feature engineering - length, lowercase count, uppercase count, digit count, special count, digit_ratio, special_ratio, char_ratio,  shannon entropy, PQI rating, Zxcvbn Rating, unique_char_ratio, has_sequence, has_pattern, repeated_char_count, is_common, in_dictionary
+df["length"] = df["password"].apply(len)  # creates new column for length of each password
+df["lowercase_count"] = df["password"].apply(lambda f: sum(1 for s in f if s.islower()))  # creates new column for lowercase alphabetic character count
+df["uppercase_count"] = df["password"].apply(lambda f: sum(1 for s in f if s.isupper()))  # creates new column for uppercase alphabetic character count
+df["digit_count"] = df["password"].apply(lambda f: sum(1 for s in f if s.isdigit()))  # creates new column for uppercase alphabetic character count
+df["special_count"] = df["password"].apply(lambda f: sum(1 for s in f if s in string.punctuation)) # creates new column for count of number of special characters
 
-df['length'] = df['password'].apply(len) # creates now column for length of each password
-
+# display results
+pd.set_option("display.max_columns", None) # displays each column used for the time being
 print(df.head())  # first few rows
 print(df.info())  # info
 print(df.isnull().sum())  # missing values
+print(df.loc[[0]])  # prints a specific entry
