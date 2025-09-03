608e202dafb54cd42bd89ab2d0059a7a72fc98ef
Pr0xy08 <drew.wandless1@gmail.com>
2025-09-03 11:22:24 +0100

    added char_diversity using Simpson Index calculation

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import string
 import math
+from zxcvbn import zxcvbn  # maybe use - also maybe use PQI (password Quality Indicator)
 
 
 def shannon_entropy(pwd):
     prob = [pwd.count(c) / len(pwd) for c in set(pwd)]
-    return -sum(p * math.log2(p) for p in prob)
+    return -sum(p * math.log2(p) for p in prob) # Shannon Index
+
+
+def char_diversity(pwd: str):
+    prob = [pwd.count(c) / len(pwd) for c in set(pwd)]
+    return 1 - sum(f ** 2 for f in prob)  # Simpson Index
 
 
 # Read the CSV safely
@@ -26,7 +32,7 @@ df = df.drop_duplicates(subset=["password"])
 # reset index
 df = df.reset_index(drop=True)
 
-# feature engineering - length, lowercase count, uppercase count, digit count, special count, digit_ratio, special_ratio, char_ratio,  shannon entropy, PQI rating, Zxcvbn Rating, unique_char_ratio, has_sequence, has_pattern, repeated_char_count, is_common, in_dictionary
+# General Features
 df["length"] = df["password"].apply(len)  # creates new column for length of each password
 df["lowercase_count"] = df["password"].apply(
     lambda f: sum(1 for s in f if s.islower()))  # creates new column for lowercase alphabetic character count
@@ -39,15 +45,19 @@ df["special_count"] = df["password"].apply(lambda f: sum(
 df["unique_count"] = df["password"].apply(
     lambda f: len(set(f)))  # creates a new column that counts the number of unique characters
 
-# Ratio features
+# Ratio Features
 df["lowercase_ratio"] = df["lowercase_count"] / df["length"]
 df["uppercase_ratio"] = df["uppercase_count"] / df["length"]
 df["digit_ratio"] = df["digit_count"] / df["length"]
 df["special_ratio"] = df["special_count"] / df["length"]
 df["unique_ratio"] = df["unique_count"] / df["length"]
 
-# Password Strength Metric Features
-df['shannon_entropy'] = df['password'].apply(shannon_entropy)
+# Randomness
+df["shannon_entropy"] = df["password"].apply(shannon_entropy)  # creates a new column providing shannon entropy for each of the passwords
+df["char_diversity"] = df["password"].apply(char_diversity)
+
+# Pattern Features
+
 
 # display results
 pd.set_option("display.max_columns", None)  # displays each column used for the time being
